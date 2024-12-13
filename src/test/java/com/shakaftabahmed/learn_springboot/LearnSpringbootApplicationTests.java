a38922283e5452bec8eedbfd20721d8853b6ad45
aftab shaik <2010040116@klh.edu.in>
2024-12-13 11:26:37 +0530

    first commit

---
+++ src/test/java/com/shakaftabahmed/learn_springboot/LearnSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shakaftabahmed.learn_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
